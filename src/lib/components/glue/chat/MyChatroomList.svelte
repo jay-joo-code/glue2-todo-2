6c2b0a23419eab7b5537c3382c95d1fb081339d4
Jay Joo <jj534@cornell.edu>
2023-03-26 11:02:57 -0400

    add loading, empty chatroom list states

--- src/lib/components/glue/chat/MyChatroomList.svelte
+++ src/lib/components/glue/chat/MyChatroomList.svelte
@@ -4,10 +4,12 @@
 	import getOtherUser from '$lib/util/glue/chat/getOtherUser';
 	import dynamicAgo from '$lib/util/glue/dynamicAgo';
 	import { onDestroy, onMount } from 'svelte';
+	import Loading from '../Loading.svelte';
 
 	export let activeChatroomId;
 	let chatrooms = [];
 	let unsubscribe: () => void;
+	let isLoading = false;
 
 	const appendChat = async (chat) => {
 		try {
@@ -63,6 +65,8 @@
 	};
 
 	const fetchChatrooms = async () => {
+		isLoading = true;
+
 		try {
 			const chatroomDocs = await pb.collection('chatrooms').getFullList(200, {
 				filter: `author="${$currentUser?.id}"||searcher="${$currentUser?.id}"`,
@@ -94,6 +98,8 @@
 						new Date(a?.latestChat?.created)?.getTime()
 				);
 		} catch (error) {}
+
+		isLoading = false;
 	};
 
 	onMount(() => {
@@ -113,43 +119,49 @@
 			You'll receive an email notification when someone messages you!
 		</p>
 	</div>
-	{#each chatrooms as chatroom (chatroom?.id)}
-		<a href={`/chatrooms/${chatroom?.id}`}>
-			<div
-				class={`border-b border-base-content/20 p-4 pt-2 ${
-					activeChatroomId === chatroom?.id && 'bg-base-300'
-				}`}
+	{#if isLoading}
+		<div class="flex justify-center">
+			<Loading />
+		</div>
+	{:else if chatrooms?.length > 0}
+		{#each chatrooms as chatroom (chatroom?.id)}
+			<a href={`/chatrooms/${chatroom?.id}`}>
+				<div
+					class={`border-b border-base-content/20 p-4 pt-2 ${
+						activeChatroomId === chatroom?.id && 'bg-base-300'
+					}`}
+				>
+					<div class="flex items-center space-x-3">
+						<!-- avatar -->
+						<div class="avatar">
+							<div class="w-12 rounded-full">
+								<img src={chatroom?.otherUser?.avatarUrl} />
+							</div>
+						</div>
+
+						<!-- info text -->
+						<div class="space-y-0.5 overflow-hidden">
+							<!-- top: name, origin, destination -->
+							<div class="flex items-center">
+								<p class="truncate font-medium">
+									{chatroom?.otherUser?.name}
+								</p>
+							</div>
+						</div>
+					</div>
+				</div></a
 			>
-				<div class="flex items-center space-x-3">
-					<!-- avatar -->
-					<div class="avatar">
-						<div class="w-12 rounded-full">
-							<img src={chatroom?.otherUser?.avatarUrl} />
-						</div>
-					</div>
-
-					<!-- info text -->
-					<div class="space-y-0.5 overflow-hidden">
-						<!-- top: name, origin, destination -->
-						<div class="flex items-center">
-							<p class="truncate font-medium">
-								{chatroom?.otherUser?.name}
-							</p>
-						</div>
-
-						<!-- middle: recent chat -->
-						<p class="truncate text-xs text-base-content/90">
-							{chatroom?.latestChat?.sender === $currentUser?.id ? 'You: ' : ''}{chatroom
-								?.latestChat?.content}
-						</p>
-
-						<!-- bottom: date -->
-						<p class="w-full truncate text-xs text-base-content/70">
-							{dynamicAgo({ date: new Date(chatroom?.latestChat?.created) })}
-						</p>
-					</div>
+		{/each}
+	{:else}
+		<div class="flex w-full justify-center px-3">
+			<div class="flex w-full items-center justify-center rounded-xl bg-base-200 py-4">
+				<div class="w-4/6">
+					<p class="text-md font-semibold">No chatrooms yet!</p>
+					<p class="mt-2 text-sm text-base-content/70">
+						Send messages to and see your chatrooms here
+					</p>
 				</div>
 			</div>
-		</a>
-	{/each}
+		</div>
+	{/if}
 </div>
